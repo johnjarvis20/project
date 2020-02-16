a2ef7e095c115ef8249ee53e523eb4841d0f8be1
John Jarvis De Guzman <61111749+johnjarvis20@users.noreply.github.com>
2020-02-16 23:41:25 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function billingFunction() {
+    var form = {
+      same: document.getElementById('same'),
+      shippingInfo: {
+        name: document.getElementById('shippingName'),
+        zip: document.getElementById('shippingZip')
+      },    
+      billingInfo: {
+        name: document.getElementById('billingName'),
+        zip: document.getElementById('billingZip')
+      }        
+    };  
+    
+    if (form.same.checked) {
+      for (prop in form.shippingInfo) {
+        if (form.shippingInfo.hasOwnProperty(prop)) {
+          form.billingInfo[prop].value = form.shippingInfo[prop].value;
+        }
+      }
+    } else  {
+      for (prop in form.billingInfo) {
+        if (form.billingInfo.hasOwnProperty(prop)) {
+          form.billingInfo[prop].value = ""; }   }    
+    }
+  };
